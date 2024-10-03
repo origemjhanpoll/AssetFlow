38066258084eaacc8a962e4351441cc83a6152d4
Jean Paul <origemjhanpoll@gmail.com>
2024-10-02 21:26:54 -0300

    remove commits

--- lib/domain/usecases/get_tree.dart
+++ lib/domain/usecases/get_tree.dart
@@ -12,7 +12,6 @@ class GetTree {
     var locations = await repository.getLocations(companyId: companyId);
     var assets = await repository.getAssets(companyId: companyId);
 
-    // Atribui os tipos corretos a assets e locations
     assets = assets
         .map((element) => element.copyWith(type: setType(element)))
         .toList();
@@ -21,22 +20,15 @@ class GetTree {
         .map((element) => element.copyWith(type: ItemType.location))
         .toList();
 
-    // Junta os assets e locations
     final branchs = [...assets, ...locations];
-
-    // Organiza a árvore
-    List<Branch> tree = _buildTree(branchs);
+    final tree = _buildTree(branchs);
 
     return tree;
   }
 }
 
-// Função para organizar a árvore
 List<Branch> _buildTree(List<Branch> branchs) {
-  // Mapa para armazenar elementos por id
   Map<String, Branch> map = {for (var item in branchs) item.id: item};
-
-  // Lista de elementos principais (sem parentId ou locationId)
   List<Branch> tree = [];
 
   for (var item in branchs) {
@@ -44,13 +36,10 @@ List<Branch> _buildTree(List<Branch> branchs) {
     var locationId = item.locationId;
 
     if (parentId != null && map.containsKey(parentId)) {
-      // Adiciona o item como filho na chave "branch" do elemento pai
       map[parentId]?.branches.add(item);
     } else if (locationId != null && map.containsKey(locationId)) {
-      // Adiciona o item como filho na chave "branch" do elemento location
       map[locationId]?.branches.add(item);
     } else {
-      // Caso não tenha parentId nem locationId, é um nó raiz
       tree.add(item);
     }
   }
